996d27b344e81a57c9f4351f86b4deb93f038325
yo0o0n <137882696+yo0o0n@users.noreply.github.com>
2024-10-25 18:00:19 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 4536 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -0,0 +1,44 @@
+#include <iostream> 
+#include <vector> 
+#include <string>
+#include <queue> 
+#include <algorithm>
+using namespace std;
+
+const int MAX = 501;
+int height[MAX][MAX];
+int dp[MAX][MAX];
+int m, n;
+int cnt = 0;
+int dx[] = { 0, 0, -1, 1 };
+int dy[] = { -1, 1, 0, 0 };
+
+int dfs(int x, int y) {
+	if (x == m - 1 && y == n - 1) return 1;
+	if (dp[x][y] != -1) return dp[x][y];
+	dp[x][y] = 0;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx < 0 || nx >= m || ny < 0 || ny >= n)	continue;
+		if (height[x][y] > height[nx][ny]) {
+			dp[x][y] += dfs(nx, ny);
+		}
+	}
+	return dp[x][y]; 
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin >> m >> n;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> height[i][j];
+			dp[i][j] = -1;
+		}
+	}
+	cout << dfs(0, 0);
+	return 0;
+}
