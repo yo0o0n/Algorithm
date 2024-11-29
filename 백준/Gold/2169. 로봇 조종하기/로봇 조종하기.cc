1110667a8f857ea767aef278c943345b6ed9aa53
yo0o0n <137882696+yo0o0n@users.noreply.github.com>
2024-11-29 14:08:33 +0900

    [Gold II] Title: 로봇 조종하기, Time: 76 ms, Memory: 9856 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.cc
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.cc
@@ -1,39 +1,32 @@
 #include<iostream>
 using namespace std;
-int N, M;
+
+int n, m;
 int map[1001][1001];
 int dp[1001][1001];
-int Right[1001];
-int Left[1001];
-
+int R[1001];
+int L[1001];
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++)
 			cin >> map[i][j];
-		}
 	}
 	dp[0][0] = map[0][0];
-	for (int j = 1; j < M; j++) // 맨 윗줄 dp 생성
+	for (int j = 1; j < m; j++)		// 맨 윗줄 dp 생성
 		dp[0][j] += dp[0][j - 1] + map[0][j];
-
-	for (int i = 1; i < N; i++)
-	{
-		// 왼쪽 -> 오른쪽
-		Right[0] = dp[i - 1][0] + map[i][0];
-		for (int j = 0; j < M - 1; j++)
-			Right[j + 1] = max(Right[j], dp[i - 1][j + 1]) + map[i][j + 1];
-
-		// 오른쪽 -> 왼쪽
-		Left[M - 1] = dp[i - 1][M - 1] + map[i][M - 1];
-		for (int j = M - 2; j >= 0; j--)
-			Left[j] = max(Left[j + 1], dp[i - 1][j]) + map[i][j];
-
-		for (int k = 0; k < M; k++)
-			dp[i][k] = max(Right[k], Left[k]);
+	for (int i = 1; i < n; i++) {
+		R[0] = dp[i - 1][0] + map[i][0];	// L -> R
+		for (int j = 0; j < m - 1; j++)
+			R[j + 1] = max(R[j], dp[i - 1][j + 1]) + map[i][j + 1];
+		L[m - 1] = dp[i - 1][m - 1] + map[i][m - 1];	// R -> L
+		for (int j = m - 2; j >= 0; j--)
+			L[j] = max(L[j + 1], dp[i - 1][j]) + map[i][j];
+		for (int k = 0; k < m; k++)
+			dp[i][k] = max(R[k], L[k]);
 	}
-	cout << dp[N - 1][M - 1];
+	cout << dp[n - 1][m - 1];
 	return 0;
 }
