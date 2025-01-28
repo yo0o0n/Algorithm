24fc9429b2547be593e71ae718a33bc1194c6923
yo0o0n <137882696+yo0o0n@users.noreply.github.com>
2025-01-28 13:32:35 +0900

    [Gold II] Title: 성냥개비, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.cc
+++ 백준/Gold/3687. 성냥개비/성냥개비.cc
@@ -3,16 +3,14 @@
 #include<vector>
 using namespace std;
 typedef long long ll;
-
 ll dp[101] = { 0, };
-int num[9] = { 0,0,1,7,4,2,0,8,10 };
+int num[9] = { 0, 0, 1, 7, 4, 2, 0, 8, 10 };
 int tc;
 
 void sol() {    // 최소값 메모이제이션 -> 배열에 저장
     for (int i = 1; i <= 8; i++)
         dp[i] = num[i];
     dp[6] = 6;
-
     for (int i = 9; i <= 100; i++) {
         dp[i] = 888888888888888;
         for (int j = 2; j < 8; j++) {
@@ -30,7 +28,6 @@ int main(void) {
         int n;
         cin >> n;
         cout << dp[n] << " ";
-
         while(n) {
             if (n % 2 != 0) {
                 cout << 7;
